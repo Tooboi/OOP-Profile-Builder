9f06c657ba793ba5cc76ca684821c57273b63f82
Tooboi <japica123@gmail.com>
2022-12-07 00:48:54 -0500

    prompt logic

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
+const { listenerCount } = require("process");
 
 // team array
 const teamArray = [];
@@ -34,43 +35,112 @@ const createManager = () => {
   return inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What's your team manager's name?",
+        type: 'input',
+        name: 'name',
+        message: "\u001b[31;1mWhat's your team manager's name?",
         validate: (nameInput) => {
           if (nameInput) {
             return true;
           } else {
-            console.log("Please enter the manager's name!");
+            console.log("\u001b[34;1m Please enter the manager's name!");
             return false;
           }
         }
       },
       {
-        type: "input",
-        name: "id",
-        message: "What's your team manager's ID?",
+        type: 'input',
+        name: 'id',
+        message: "\u001b[31;1mWhat's your team manager's ID?",
       },
       {
-        type: "input",
-        name: "email",
-        message: "What's your team manager's email address?",
+        type: 'input',
+        name: 'email',
+        message: "\u001b[31;1mWhat's your team manager's email address?",
       },
       {
-        type: "input",
-        name: "officeNumber",
-        message: "What's your team manager's office number?",
+        type: 'input',
+        name: 'officeNumber',
+        message: "\u001b[31;1mWhat's your team manager's office number?",
       },
+      {
+        type: 'confirm',
+        name: 'role',
+        message: "\u001b[31;1mWould you like to add another employee?",
+        choices: ['Yes', "Nope, I'm done"],
+        validate: roleInput => {
+            if (roleInput === "Nope, I'm done") {
+                return
+            }
+        }
+    },
     ])
     .then(managerInput => {
-        console.log("Manager added!");
+        console.log(
+`\u001b[34;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          \u001b[35;1mAdded to the team!
+\u001b[34;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);        
         const { name, id, email, officeNumber } = managerInput;
         const manager = new Manager(name, id, email, officeNumber);
         teamArray.push(manager)
-        console.log(teamArray);
     });
 };
-console.log(teamArray);
 
-// john.getRole();
+const newEmployee = () => {
+
+return inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'role',
+        message: "\u001b[31;1mWould you like to add another employee?",
+        choices: ['Intern', 'Engineer', "Nope, I'm done"],
+        validate: roleInput => {
+            if (roleInput === "Nope, I'm done") {
+                return
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "\u001b[31;1mWhat's your employees name?",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "\u001b[31;1mWhat's your employees ID?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "\u001b[31;1mWhat's your employees email address?",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "\u001b[31;1mWhere did your employee go to school?",
+        when: (input) => input.role === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "\u001b[31;1mWhat's your employees GitHub username?",
+        when: (input) => input.role === 'Engineer'
+    }
+])
+.then(employeeInfo => {
+    let { name, id, email, role, github, school, } = employeeInfo
+})
+}
+
+
+// write html file
+
+
+
+
 createManager()
+    .then(newEmployee)
+    // .then(teamArray => {
+    //     return generateHTML(teamArray);
+    // })
+    // .then()
