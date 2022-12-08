f72a59ef7831b9125d908c957e36efbb7e0856b6
Tooboi <japica123@gmail.com>
2022-12-08 01:46:03 -0500

    propmt creates styled html page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,10 +3,11 @@ const generateManager = function (manager) {
   return `<figure class="custom-card m-4 ">
   <div class="card-body">
     <h5 class="card-title"><i class="fa-solid fa-gear"></i> ${manager.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
-    <p class="card-text">ID: ${manager.id}</p>
-    <p><a href="#" class="card-link">${manager.email}</a></p>
-    <p class="card-link">Office: ${manager.officeNum}</p>
+    <h6 class="card-subtitle mb-3 custom-text-muted">Manager</h6>
+  <hr>
+    <p class="card-text mb-2">ID: ${manager.id}</p>
+    <p><a href="#" class="card-text mb-0">${manager.email}</a></p>
+    <p class="card-text mb-0">Office: ${manager.officeNum}</p>
   </div>
 </figure>`
 }
@@ -15,10 +16,11 @@ const generateIntern = function (intern) {
   return `<figure class="custom-card m-4 ">
   <div class="card-body">
     <h5 class="card-title"><i class="fa-solid fa-face-smile"></i> ${intern.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Intern</h6>
-    <p class="card-text">ID: ${intern.id}</p>
-    <p><a href="#" class="card-link">${intern.email}</a></p>
-    <p class="card-link">School: ${intern.school}</p>
+    <h6 class="card-subtitle mb-3 custom-text-muted">Intern</h6>
+    <hr>
+    <p class="card-text mb-2">ID: ${intern.id}</p>
+    <p><a href="#" class="card-link mb-0">${intern.email}</a></p>
+    <p class="card-link mb-0">School: ${intern.school}</p>
   </div>
 </figure>`
 };
@@ -27,10 +29,11 @@ const generateEngineer = function (engineer) {
   return `<figure class="custom-card m-4 ">
   <div class="card-body">
     <h5 class="card-title"><i class="fa-solid fa-glasses"></i> ${engineer.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
-    <p class="card-text">ID: ${engineer.id}</p>
-    <p><a href="#" class="card-link">${engineer.email}</a></p>
-    <p class="card-link">GitHub: <a href="https://github.com/${engineer.github}" class="card-link">${engineer.github}</a></p>
+    <h6 class="card-subtitle mb-3 custom-text-muted">Engineer</h6>
+    <hr>
+    <p class="card-text mb-2">ID: ${engineer.id}</p>
+    <p><a href="#" class="card-text mb-0">${engineer.email}</a></p>
+    <p class="card-text mb-0">GitHub: <a href="https://github.com/${engineer.github}" class="card-link">${engineer.github}</a></p>
   </div>
 </figure>`
 };
@@ -89,9 +92,9 @@ const makeTeamPage = function (teamCards) {
     <title>My Team</title>
   </head>
   <body>
-    <nav class="navbar bg-light">
+    <nav class="custom-navbar">
       <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Current Team</span>
+        <span class="navbar-brand mb-0 h1">My Team</span>
       </div>
     </nav>
     <main class="d-flex flex-wrap justify-content-center">
